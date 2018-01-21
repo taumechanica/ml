eb258edd7383786c2339c52899e903bc88766b90
taumechanica <eugen.steppenwolf@gmail.com>
2018-01-21 15:28:59 +0300

    DataFrame: split method

--- src/main/kotlin/ml/data/DataFrame.kt
+++ src/main/kotlin/ml/data/DataFrame.kt
@@ -3,6 +3,8 @@
 
 package taumechanica.ml.data
 
+import java.util.Collections.shuffle
+
 class Sample(val values: DoubleArray) {
     var weight: DoubleArray? = null
     var actual: DoubleArray? = null
@@ -14,7 +16,23 @@ class DataFrame(
     val features: Array<Attribute>,
     val samples: Array<Sample>,
     val subset: BooleanArray,
-    val filter: BooleanArray
+    val filter: BooleanArray?
 ) {
-    
+    public fun split(ratio: Double): Pair<DataFrame, DataFrame> {
+        if (ratio <= 0.0 || ratio > 1.0) {
+            throw Exception("Invalid split ratio")
+        }
+
+        val indices = mutableListOf<Int>()
+        for (i in 0 until samples.size) if (subset[i]) indices.add(i)
+
+        val permutation = shuffle(indices)
+        val first = DataFrame(target, features, samples, subset.copyOf(), null)
+        val second = DataFrame(target, features, samples, subset.copyOf(), null)
+        val size = Math.ceil(indices.size * ratio).toInt()
+        for (i in indices.size - size + 1 until indices.size) first.subset[i] = false
+        for (i in 0 until size) second.subset[i] = false
+
+        return Pair(first, second)
+    }
 }
