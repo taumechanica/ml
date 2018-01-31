e2fc8a2629dc66a6c2a8ed647156b7ae5e7aa799
taumechanica <eugen.steppenwolf@gmail.com>
2018-01-31 10:33:58 +0300

    Product meta-operator based on weighted Hamming loss function

---
+++ src/main/kotlin/ml/meta/HProduct.kt
@@ -0,0 +1,77 @@
+// Use of this source code is governed by The MIT License
+// that can be found in the LICENSE file.
+
+package taumechanica.ml.meta
+
+import taumechanica.ml.*
+import taumechanica.ml.base.HUnit
+import taumechanica.ml.data.DataFrame
+
+class HProduct : Classifier {
+    override val alpha: Double
+    override val gamma: Double
+
+    override val votes: DoubleArray
+
+    val factors: Array<Classifier>
+
+    constructor(frame: DataFrame, strategy: Strategy, complexity: Int) {
+        if (complexity < 2) throw Exception(
+            "Product complexity should be greater or equals 2"
+        )
+
+        votes = DoubleArray(frame.target.size, { 0.0 })
+        factors = Array(complexity, { HUnit(frame) })
+
+        frame.resetTargets()
+
+        val indices = mutableListOf<Int>()
+        for (i in 0 until frame.samples.size) {
+            if (frame.subset[i]) indices.add(i)
+        }
+
+        var j = 0
+        var bestAlpha = 0.0
+        var bestGamma = 0.0
+        while (true) {
+            for (k in 0 until frame.target.size) votes[k] = 1.0
+            for (factor in factors) {
+                for (k in 0 until frame.target.size) {
+                    votes[k] *= factor.votes[k]
+                }
+            }
+
+            for (i in indices) {
+                val sample = frame.samples[i]
+                val numerator = predict(sample.values)
+                val denominator = factors[j].predict(sample.values)
+                for (k in 0 until frame.target.size) {
+                    sample.target[k] = sample.actual[k] * (
+                        if (numerator[k] / denominator[k] > 0) 1.0 else -1.0
+                    )
+                }
+            }
+
+            val factor = strategy.fit(frame)
+            if (factor.gamma <= bestGamma) break
+
+            bestAlpha = factor.alpha
+            bestGamma = factor.gamma
+            factors[j] = factor
+
+            j = if (j == complexity - 1) 0 else j + 1
+        }
+
+        alpha = bestAlpha
+        gamma = bestGamma
+    }
+
+    override fun predict(values: DoubleArray): DoubleArray {
+        val phi = phi(values)
+        return DoubleArray(votes.size, { alpha * votes[it] * phi })
+    }
+
+    override fun phi(values: DoubleArray): Int {
+        return IntArray(factors.size, { factors[it].phi(values) }).reduce { prev, curr -> curr * prev }
+    }
+}
